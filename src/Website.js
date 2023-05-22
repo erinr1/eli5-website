2058e55f2a4b63dd01f1f955223c983bb338b933
erinr1 <erin.ramos87@gmail.com>
2023-05-22 13:08:43 -0400

    added scrolltotop component

--- src/Website.js
+++ src/Website.js
@@ -4,16 +4,19 @@ import Disclaimer from "./Disclaimer";
 import { Routes, Route } from "react-router-dom";
 import PrivacyPolicy from "./PrivacyPolicy";
 import TermsService from "./TermsService";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Website() {
   return (
     <div className="Website">
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/disclaimer" element={<Disclaimer />} />
         <Route path="/privacypolicy" element={<PrivacyPolicy />} />
         <Route path="/termsservice" element={<TermsService />} />
       </Routes>
+      <ScrollToTop />
     </div>
   );
 }
